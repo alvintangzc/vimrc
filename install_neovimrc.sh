add5681bd9cf785ee6146a5121651e27e9496f18
alvintangzc <4008321@qq.com>
2018-10-09 15:19:20 +0800

    merge ycm

--- install_neovimrc.sh
+++ install_neovimrc.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 set -e
 
+mkdir -p ~/.config/nvim
 cd ~/.config
-cp ~/.config/nvim/init.vim ~/.config/nvim/init.vim.bak
+
+if [ -f " ~/.config/nvim/init.vim" ];then
+    cp ~/.config/nvim/init.vim ~/.config/nvim/init.vim.bak
+fi
+
+#    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+#        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 echo 'set runtimepath+=~/.config/vimrc
 
@@ -10,7 +17,8 @@ let g:python_host_prog = "/usr/bin/python"
 let g:python3_host_prog = "/usr/local/bin/python3"
 
 source ~/.config/vimrc/basic.vim
-source ~/.config/vimrc/plugins_config.vim 
+source ~/.config/vimrc/plugins_config.vim
 source ~/.config/vimrc/shortcuts.vim ' > ~/.config/nvim/init.vim
+
 
 echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
